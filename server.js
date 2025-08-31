5970befa58faaaead77a1d7369ae5418c308abd6
Yuvarani752004 <147576485+Yuvarani752004@users.noreply.github.com>
2025-08-31 11:13:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -67,6 +67,6 @@ app.listen(PORT, () => {
 });
 
 mongoose
-  .connect("mongodb://localhost:27017/yuva", {})
+  .connect(process.env.MONGODBURL, {})
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
